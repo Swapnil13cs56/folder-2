ba554c006ca46efd1494b3510ce9590a1109abc7
Swapnil kumar <Swapnil13cs56@gmail.com>
2021-10-13 14:39:21 +0530

    Add files via upload

---
+++ sketch_oct13a.ino
@@ -0,0 +1,187 @@
+//ArduinoMega Build
+
+void setup() 
+{
+  pinMode(2, OUTPUT); 
+  pinMode(3, OUTPUT); 
+  pinMode(4, OUTPUT); 
+  pinMode(5, OUTPUT); 
+  pinMode(6, OUTPUT); 
+  pinMode(7, OUTPUT); 
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT); 
+  pinMode(13, OUTPUT); 
+  pinMode(14, OUTPUT); 
+  pinMode(15, OUTPUT); 
+  pinMode(16, OUTPUT); 
+  pinMode(17, OUTPUT); 
+  pinMode(18, OUTPUT); 
+  pinMode(19, OUTPUT); 
+  pinMode(20, OUTPUT); 
+  pinMode(21, OUTPUT); 
+  pinMode(22, OUTPUT); 
+  pinMode(23, OUTPUT); 
+  pinMode(24, OUTPUT); 
+  pinMode(25, OUTPUT); 
+  pinMode(26, OUTPUT); 
+  pinMode(27, OUTPUT); 
+
+}
+
+void loop() 
+{
+
+  digitalWrite(10, HIGH);   
+  delay(1000);                       
+  digitalWrite(10, LOW);    
+  delay(1000);  
+
+  digitalWrite(16, HIGH);   
+  delay(1000);                       
+  digitalWrite(16, LOW);    
+  delay(1000);
+
+  digitalWrite(21, HIGH);   
+  delay(1000);                       
+  digitalWrite(21, LOW);    
+  delay(1000);
+
+  digitalWrite(17, HIGH);   
+  delay(1000);                       
+  digitalWrite(17, LOW);    
+  delay(1000);
+
+  digitalWrite(19, HIGH);   
+  delay(1000);                       
+  digitalWrite(19, LOW);    
+  delay(1000);
+
+  digitalWrite(16, HIGH);   
+  delay(1000);                       
+  digitalWrite(16, LOW);    
+  delay(1000);
+
+  digitalWrite(8, HIGH);   
+  delay(1000);                       
+  digitalWrite(8, LOW);    
+  delay(1000);
+
+  digitalWrite(19, HIGH);   
+  delay(1000);                       
+  digitalWrite(19, LOW);    
+  delay(1000);
+
+  digitalWrite(2, HIGH);   
+  delay(1000);                       
+  digitalWrite(2, LOW);    
+  delay(1000);
+
+  digitalWrite(14, HIGH);   
+  delay(1000);                       
+  digitalWrite(14, LOW);    
+  delay(1000);
+
+  digitalWrite(10, HIGH);   
+  delay(1000);                       
+  digitalWrite(10, LOW);    
+  delay(1000);
+
+  digitalWrite(15, HIGH);   
+  delay(1000);                       
+  digitalWrite(15, LOW);    
+  delay(1000);
+
+  digitalWrite(8, HIGH);   
+  delay(1000);                       
+  digitalWrite(8, LOW);    
+  delay(1000);
+  
+  digitalWrite(10, HIGH);   
+  delay(1000);                       
+  digitalWrite(10, LOW);    
+  delay(1000);
+  
+  digitalWrite(20, HIGH);   
+  delay(1000);                       
+  digitalWrite(20, LOW);    
+  delay(1000);
+  
+  digitalWrite(23, HIGH);   
+  delay(1000);                       
+  digitalWrite(23, LOW);    
+  delay(1000);
+  
+  digitalWrite(6, HIGH);   
+  delay(1000);                       
+  digitalWrite(6, LOW);    
+  delay(1000);
+  
+  digitalWrite(19, HIGH);   
+  delay(1000);                       
+  digitalWrite(19, LOW);    
+  delay(1000);
+
+  digitalWrite(26, HIGH);   
+  delay(1000);                       
+  digitalWrite(26, LOW);    
+  delay(1000);
+
+  digitalWrite(10, HIGH);   
+  delay(1000);                       
+  digitalWrite(10, LOW);    
+  delay(1000);
+
+  digitalWrite(15, HIGH);   
+  delay(1000);                       
+  digitalWrite(15, LOW);    
+  delay(1000);
+
+  digitalWrite(21, HIGH);   
+  delay(1000);                       
+  digitalWrite(21, LOW);    
+  delay(1000);
+
+  digitalWrite(6, HIGH);   
+  delay(1000);                       
+  digitalWrite(6, LOW);    
+  delay(1000);
+
+  digitalWrite(19, HIGH);   
+  delay(1000);                       
+  digitalWrite(19, LOW);    
+  delay(1000);
+
+  digitalWrite(6, HIGH);   
+  delay(1000);                       
+  digitalWrite(6, LOW);    
+  delay(1000);
+
+  digitalWrite(20, HIGH);   
+  delay(1000);                       
+  digitalWrite(20, LOW);    
+  delay(1000);
+
+  digitalWrite(21, HIGH);   
+  delay(1000);                       
+  digitalWrite(21, LOW);    
+  delay(1000);
+
+  digitalWrite(10, HIGH);   
+  delay(1000);                       
+  digitalWrite(10, LOW);    
+  delay(1000);
+
+  digitalWrite(15, HIGH);   
+  delay(1000);                       
+  digitalWrite(15, LOW);    
+  delay(1000);
+
+  digitalWrite(8, HIGH);   
+  delay(1000);                       
+  digitalWrite(8, LOW);    
+  delay(1000);
+
+}
